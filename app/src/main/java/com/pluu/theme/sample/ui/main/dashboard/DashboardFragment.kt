687bf061b1f91662174d53ebcda42fa84e038945
pluu <pluulove@gmail.com>
2024-03-27 23:40:28 +0900

    로직 정리

--- app/src/main/java/com/pluu/theme/sample/ui/main/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/pluu/theme/sample/ui/main/dashboard/DashboardFragment.kt
@@ -4,35 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.pluu.theme.sample.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val viewModel by viewModels<DashboardViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.text.observe(viewLifecycleOwner) { text ->
+            binding.textDashboard.text = text
         }
-        return root
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/pluu/theme/sample/ui/main/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/pluu/theme/sample/ui/main/dashboard/DashboardViewModel.kt
@@ -3,11 +3,17 @@ package com.pluu.theme.sample.ui.main.dashboard
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import timber.log.Timber
+import javax.inject.Inject
 
-class DashboardViewModel : ViewModel() {
+@HiltViewModel
+class DashboardViewModel @Inject constructor() : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
-    }
+    private val _text = MutableLiveData("This is dashboard Fragment")
     val text: LiveData<String> = _text
+
+    init {
+        Timber.d("HashCode=[${hashCode()}]")
+    }
 }
