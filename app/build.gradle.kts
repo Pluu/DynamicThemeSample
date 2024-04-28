b7e85cc57930e37210370412ae1c1eeeae7577e1
pluu <pluulove@gmail.com>
2024-04-28 21:30:37 +0900

    Modify minSdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.pluu.theme.sample"
-        minSdk = 24
+        minSdk = 23
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
