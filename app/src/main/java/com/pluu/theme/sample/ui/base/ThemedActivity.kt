191be1a0b4e3cdb7c11fe561b73e76f911b121de
pluu <pluulove@gmail.com>
2024-05-17 01:04:16 +0900

    Update color/theme2

--- app/src/main/java/com/pluu/theme/sample/ui/base/ThemedActivity.kt
+++ app/src/main/java/com/pluu/theme/sample/ui/base/ThemedActivity.kt
@@ -2,6 +2,7 @@ package com.pluu.theme.sample.ui.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import com.pluu.theme.sample.R
 import com.pluu.theme.sample.di.ThemedActivityDelegateInterface
 import com.pluu.theme.sample.model.Theme
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,7 +16,10 @@ abstract class ThemedActivity : AppBaseActivity() {
     protected val currentTheme: Theme
         get() = themedActivityDelegate.currentTheme
 
+    private var updateThemeTranslation = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        updateThemeTranslation = false
         initDelegate()
         super.onCreate(savedInstanceState)
     }
@@ -34,8 +38,16 @@ abstract class ThemedActivity : AppBaseActivity() {
     }
 
     override fun onResume() {
-        initializeTheme()
         super.onResume()
+        updateThemeTranslation = true
+        initializeTheme()
+    }
+
+    override fun onNightModeChanged(mode: Int) {
+        super.onNightModeChanged(mode)
+        if (updateThemeTranslation) {
+            overridePendingTransition(R.anim.short_fade_in, R.anim.short_fade_out)
+        }
     }
 
     private fun getNightMode(): Int {
